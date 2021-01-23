0e021f2a8ab8adef172c1042e2acf48c643c8fda
tnicacio <nicacio.t92@gmail.com>
2021-01-23 19:13:55 -0300

    GET /orders

---
+++ backend/src/main/java/com/tnicacio/dsdeliver/services/OrderService.java
@@ -0,0 +1,25 @@
+package com.tnicacio.dsdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.tnicacio.dsdeliver.dto.OrderDTO;
+import com.tnicacio.dsdeliver.entities.Order;
+import com.tnicacio.dsdeliver.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll(){
+		List<Order> list = repository.findOrdersWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+}
