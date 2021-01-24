26e889b90714fe1dfa1b630b0339a9883797dc3a
tnicacio <nicacio.t92@gmail.com>
2021-01-24 15:35:02 -0300

    Adding NavBar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import NavBar from './Navbar';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
